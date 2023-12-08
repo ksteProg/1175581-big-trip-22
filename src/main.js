e1dd4f28978a33d983ef935a4592a299b74c6a90
Karen <kste.prog@gmail.com>
2023-12-08 22:36:58 +0300

    отрисовал компоненты используя презентер

--- src/main.js
+++ src/main.js
@@ -1,13 +1,11 @@
-import FilterView from './view/filter-view.js';
-import SortView from './view/sort-view.js';
-import EditFormView from './view/edit-form-view.js';
-import {render} from './render.js';
+import TripPresenter from './presenter/trip-presenter.js';
 
 const headerElement = document.querySelector('.page-header');
 const filtersElement = headerElement.querySelector('.trip-controls__filters');
+const tripElement = headerElement.querySelector('.trip-main');
 const mainElement = document.querySelector('.page-main');
 const eventsElement = mainElement.querySelector('.trip-events');
 
-render(new FilterView(), filtersElement);
-render(new SortView(), eventsElement);
-render(new EditFormView(), eventsElement);
+const tripPresenter = new TripPresenter({ tripElement, eventsElement, filtersElement });
+
+tripPresenter.init();
